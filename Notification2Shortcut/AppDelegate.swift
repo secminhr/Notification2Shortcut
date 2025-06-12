e5c5dc8ba8aeb60ecc746faafbaa91b6a5ff8302
secminhr <secminhrian@gmail.com>
2025-06-13 00:31:59 +0800

    fix weird indent

--- Notification2Shortcut/AppDelegate.swift
+++ Notification2Shortcut/AppDelegate.swift
@@ -7,14 +7,14 @@
 
 
 import Cocoa
-   import UserNotifications
+import UserNotifications
 
-   class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
-       func applicationDidFinishLaunching(_ notification: Notification) {
-           UNUserNotificationCenter.current().delegate = self
-       }
-
-       func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-           completionHandler([.banner, .banner, .sound])
-       }
+class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCenterDelegate {
+   func applicationDidFinishLaunching(_ notification: Notification) {
+       UNUserNotificationCenter.current().delegate = self
    }
+
+   func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+       completionHandler([.banner, .banner, .sound])
+   }
+}
