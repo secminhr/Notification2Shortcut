542c509cc0c837a385b4d6a4b177ce7285450f94
secminhr <secminhrian@gmail.com>
2025-06-16 10:31:14 +0800

    extract ViewModelHolder

--- Notification2Shortcut/Notification2ShortcutApp.swift
+++ Notification2Shortcut/Notification2ShortcutApp.swift
@@ -34,25 +34,3 @@ struct Notification2ShortcutApp: App {
     }
 }
 
-struct ViewModelHolder: View {
-    @Environment(\.modelContext) private var context: ModelContext
-    @State var viewModel: NotificationsViewModel? = nil
-    @State var errorDuringLoading: Bool = false
-    
-    var body: some View {
-        if !errorDuringLoading {
-            ContentView(viewModel: viewModel)
-                .task {
-                    do {
-                        let storage = try SwiftDataStorage(modelContext: context)
-                        let manager = try await NotificationManager(storage: storage, sender: UNSender())
-                        viewModel = NotificationsViewModel(notificationManager: manager)
-                    } catch {
-                        errorDuringLoading = true
-                    }
-                }
-        } else {
-            Text("Error while loading data...")
-        }
-    }
-}
